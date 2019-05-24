4164077a31d75302e1d723bfc0d0efb6cdeae46a
iamcarp <lukakaran5@gmail.com>
2019-05-24 12:52:27 +0200

    izmenjen ram.vhd

--- NES - Zelda 1 Textures/ImageProcessing/getVHDLReadable/getVHDLReadable/getVHDLReadable.py
+++ NES - Zelda 1 Textures/ImageProcessing/getVHDLReadable/getVHDLReadable/getVHDLReadable.py
@@ -81,7 +81,7 @@ def character_sprites_to_VHDL(sprites, file_name, offset, palette_offset, max_le
     VHDL = open(file_name, "w")
     for i in range(i_range):
         #   only for loading Link sprites - skip boomerang
-        if i in range(15,21) :
+        if i in range(105) :
             continue
         VHDL.write("\n                --  sprite " + str(i) + "\n")
         temp = "        " + str(offset) + " => x\""
@@ -279,7 +279,7 @@ def generate_minimap_VHDL(file_name, offset, color):
 #character_sprites_to_VHDL(enemies_sprites, "VHDL_enemies_sprites.txt", 5072, 35)
 
 #  heart sprites
-character_sprites_to_VHDL(hearts, "VHDL_heart_sprites.txt", 1000, 40) 
+character_sprites_to_VHDL(hearts, "VHDL_heart_sprites2.txt", 8192, 40)
 
 #   Numbers and letters     -   the functions read only certain letters becaouse there is not enough memory in ram.vhd
 #values = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',',','!','\'','&','.','\"','?','-']
